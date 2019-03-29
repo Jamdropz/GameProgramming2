d84f00ad7deb7ccf4f370baa18e5b90d597c7abf
Jamdropz <46905069+Jamdropz@users.noreply.github.com>
2019-03-28 22:06:22 -0400

    Add files via upload

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+    private Color C;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        C = collision.gameObject.GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = C;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
